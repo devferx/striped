42a2a5c761aabb4d4997b288ae291c8372700963
devferx <ferkki@hotmail.com>
2022-01-13 15:48:29 -0400

    add Unified platform section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   purge: ["./*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        headlines: "#393939",
+        subtitles: "#425466",
+        white: "#ffffff",
+        purple: "#635cff",
+        grey: "#f6f9fc",
+        blue: "#01d4ff",
+        "dark-blue": "#092540",
+        "primary-buttons": "#11253e",
+        "secondary-buttons": "#0a2540",
+      },
+    },
   },
   variants: {},
   plugins: [],
